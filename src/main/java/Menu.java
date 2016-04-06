6bb582400079d03b50d14b7e9adc73180beeac10
Martendal <bouillon@et.esiea.fr>
2016-04-06 16:14:27 +0200

    Meilleur positionnement des boutons et ajout du bouton options

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -13,7 +16,7 @@ import javax.swing.SwingUtilities;
 
 public class Menu extends JPanel implements ActionListener{
 	private BufferedImage img; //Image de fond
-	private CustomButton play, tuto, quitter;
+	private CustomButton play, tuto, quitter, options;
 	private Settings set = new Settings();
 	private int rows, cols;
 	private AudioEngine musique;
@@ -28,18 +31,45 @@ public class Menu extends JPanel implements ActionListener{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//this.setLayout(null);
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
 		this.play = new CustomButton("./src/Images/BoutonPokeball.png", 50, 50);
 		this.tuto = new CustomButton("./src/Images/BoutonSuperball.png", 50, 50);
 		this.quitter = new CustomButton("./src/Images/BoutonHyperball.png", 50, 50);
-		//play.setLocation(30, 30);
+		this.options = new CustomButton("./src/Images/BoutonMasterball.png", 50, 50);
 		play.addActionListener(this);
 		tuto.addActionListener(this);
 		quitter.addActionListener(this);
+		options.addActionListener(this);
 		
-		this.add(play);
-		this.add(tuto);
-		this.add(quitter);
+		c.weightx = 1;
+		
+		//Fixe les boutons à l'ouest
+		c.anchor = GridBagConstraints.WEST;
+		
+		//Définit les marges autour des boutons
+		c.insets = new Insets(20,200,20,20);
+		
+		//Positionnement du bouton play
+		c.gridx = 0;
+		c.gridy = 0;
+		this.add(play, c);
+		
+		//Positionnement du bouton tuto
+		c.gridx = 0;
+		c.gridy = 1;
+		this.add(tuto, c);
+		
+		//Positionnement du bouton options
+		c.gridx = 0;
+		c.gridy = 2;
+		this.add(options, c);
+		
+		//Positionnement du bouton quitter
+		c.gridx = 0;
+		c.gridy = 3;
+		this.add(quitter, c);
 		
 		this.musique = new AudioEngine("Pokecenter.wav");
 		musique.start();
@@ -47,6 +77,8 @@ public class Menu extends JPanel implements ActionListener{
 	}
 	
 	
+	
+	//Gère les actions des boutons
 	@Override
 	public void actionPerformed(ActionEvent e){
 		
@@ -60,9 +92,13 @@ public class Menu extends JPanel implements ActionListener{
 		else if (e.getSource() == quitter){
 			//TODO : quitter le jeu
 		}
+		else if (e.getSource() == options){
+			//TODO : ouvrir la fenêtre de gestion des options
+		}
 	}
 	
 	
+	//Affiche l'image de fond
 	public void paintComponent(Graphics g) 
 	  { 
 		//TODO : redimensionner image
